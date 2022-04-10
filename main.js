c9bbf7da536405f7caa777e03a1c895cb87f6a03
Elvan Ünal <elvanunalll@gmail.com>
2022-04-11 00:43:56 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var card1=document.querySelector(".card1");
+var card2=document.querySelector(".card2");
+
+card1.addEventListener('click',function(){
+    document.querySelector('.container').classList.add('container-active1');  
+    
+});
+card1.addEventListener('dblclick',function(){
+    document.querySelector('.container').classList.remove('container-active1');  
+});
+card2.addEventListener('click',function(){
+    document.querySelector('.container').classList.add('container-active2');  
+});
+card2.addEventListener('dblclick',function(){
+    document.querySelector('.container').classList.remove('container-active2');  
+});
+
